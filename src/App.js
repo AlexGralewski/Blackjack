de99f03c3a3c96fb35f52c9fa3b76f61ee8740ff
AlexGralewski <gralewski.aleksander@gmail.com>
2021-01-14 20:34:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
 
 
+
   return (
     <div className="app">
-      
+      <div className="menu">
+        <button className="menu-item new-game">New Game</button>
+        <button className="menu-item load-game">Load Game</button>
+        <button className="menu-item high-scores">High Scores</button>
+      </div>
+
+      <div className="game">
+        <div className="table">
+          <div className="dealer-hand"></div>
+          <div className="deck"></div>
+          <div className="player-hand"></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/methods/displayCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function displayCard(cardCode) {
+  //takes string argument where first letter is the value of the card and second is suit
+  return(
+    <div className="card"></div>
+  )
+}
+
+export default displayCard
