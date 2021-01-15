1cd9d5c8f0665d6713fbd49bb69187d8c0b9bb7d
AlexGralewski <gralewski.aleksander@gmail.com>
2021-01-15 20:05:25 +0100

    remade app component to class

--- src/App.js
+++ src/App.js
@@ -1,51 +1,116 @@
-import React, { useState } from "react"
+import React from "react"
 
-function App() {
-  const [bet, setBet] = useState(0) //controls bet value
-  const [balance, setBalance] = useState(1000) //controls credit balance
-  const [stake, setStake] = useState(0)
-  const [playerHand, setPlayerHand] = useState('')
-  const [dealerHand, setDealerHand] = useState('')
-  const [deck, setDeck] = useState('')
-  const [displayMenu] = useState('none')
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      username: 'Player',
+      bet: 0,
+      balance: 1000,
+      stake: 0,
+      playerHand: [],
+      playerPoints: 0,
+      dealerHand: [],
+      dealerPoints: 0,
+      deck: '',
+      display: {
+        menu:'flex',
+        betSel:'none',
+        table:'none'
+      },
+      gameCount: 1,
+    }
+    this.shuffleCards = this.shuffleCards.bind(this)
+    this.dealCards = this.dealCards.bind(this)
+    this.startNewGame = this.startNewGame.bind(this)
+    this.confirmBet = this.confirmBet.bind(this)
+    this.handleBetChange = this.handleBetChange.bind(this)
+  }
 
-  function shuffleCards() {
+
+  shuffleCards() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({deck:data.deck_id}));
   }
 
-  function handleBetChange(e) {
-    setBet(e.target.value)
+  drawCards(numberofCards) {
+    fetch(`https://deckofcardsapi.com/api/deck/${this.statedeck}/draw/?count=${numberofCards}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
   }
 
-  function confirmBet(e) {
+  dealCards() {
+    const dealt = this.drawCards(4)
+
+    console.log(dealt)
+  }
+
+  handleBetChange(e) {
+    const {value} = e.target
+    this.setState({
+      bet: value
+    })
+  }
+
+  startNewGame() {
+    this.setState({
+      display: {
+        menu:'none', 
+        betSel:'flex',
+        table:'none'}
+    })
+  }
+
+  confirmBet() {
+    const { bet, balance } = this.state
     if (bet > balance || bet <= 0) {
       alert("Bet should be positive number smaller than your balance.")
     } else {
-      shuffleCards()
-      setBalance(previousBalance => {
-        return(previousBalance - bet)
+      this.shuffleCards()
+      this.setState(prevState => {
+        return({
+          balance: prevState.balance - bet,
+          stake: bet*2,
+          display: {
+            menu:'none',
+            betSel:'none',
+            table:'flex'
+          },
+        })
       })
-      setStake(bet*2)
-    }
+    } 
   }
 
-  return (
-    <div className="app">
-      <div className="menu">
-        <div className="menu-title">Blackjack</div>
-        <button className="menu-item new-game">New Game</button>
-        <button className="menu-item load-game">Load Game</button>
-        <button className="menu-item high-scores">High Scores</button>
-      </div>
-
-      <div className="game">
-        <div className="values-info">
-          <p>Stake: {stake}</p>
-          <p>Balance: {balance}</p>
+  render() {
+    const { shuffleCards, drawCards, dealCards, handleBetChange, startNewGame, confirmBet} = this
+    const {username, bet, balance, stake, playerHand, playerPoints, dealerHand, dealerPoints, deck, display, gameCount} = this.state
+    return (
+      <div className="app">
+        <div className="menu" style={{display:display.menu}}>
+          <div className="menu-title">Blackjack</div>
+          <button 
+            className="menu-item new-game"
+            onClick={startNewGame}>
+              New Game
+          </button>
+          <button 
+            className="menu-item load-game"
+            onClick={startNewGame}>
+              Load Game
+          </button>
+          <button 
+            className="menu-item high-scores"
+            onClick={startNewGame}>
+              High Scores
+          </button>
+          <button 
+            className="menu-item high-scores"
+            onClick={startNewGame}>
+              Rules
+          </button>
         </div>
-        <div className="bet-selection">
+        <div className="bet-selection" style={{display:display.betSel}}>
           <h1>BET</h1> 
           <div className="bet-buttons">         
             <input 
@@ -103,29 +168,50 @@ function App() {
             Confirm
           </button>
         </div>
-        <div className="game-table">
-          <div className="dealer-side">
-            <div className="dealer-hand"></div>
-            <div className="dealer-points"></div>
-          </div>
-          <div className="neutral">
-            <div className="deck">
-              <div className="card reversed">Deck</div>
+        <div className="game" style={{display:display.table}}>
+          
+          <div className="game-table" >
+            <div className="dealer-side">
+              <div className="player-title">Dealer</div>
+              <div className="dealer-hand"></div>
+              <div className="dealer-points">{dealerPoints}</div>
+            </div>
+            <div className="neutral">
+              <div className="values-info">
+                <p>Bet: {bet}</p>
+                <p>Stake: {stake} </p>
+                <p>Balance: {balance}</p>
+              </div>
+              <div className="deck">
+                <div className="card card1" />
+                <div className="card card2" />
+                <div className="card card3" />
+              </div>
+            </div>
+            <div className="player-side">
+              <div className="player-title">{username}</div>
+              <div className="player-hand"></div>
+              <div className="player-points">{playerPoints}</div>
             </div>
           </div>
-          <div className="player-side">
-            <div className="player-hand"></div>
-            <div className="player-points"></div>
+          <div className="game-buttons">
+            <input 
+              type="button"
+              value="Hit"
+              onClick={dealCards}/>
+            <input 
+              type="button"
+              value="Stand"
+              onClick={dealCards}/>
+            <input 
+              type="button"
+              value="Double Down"
+              onClick={dealCards}/>
           </div>
         </div>
-        <div className="game-buttons">
-          <button>Hit</button>
-          <button>Stand</button>
-          <button>Double Down</button>
-        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Rules.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Rules(props) {
+  return(
+    <ul className="Rules">
+      <li>Single player vs dealer</li>
+      <li>Decks used: 6</li>
+
+
+    </ul>
+  ) 
+}
+
+export default (Rules)
