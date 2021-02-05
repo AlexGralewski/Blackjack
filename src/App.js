f7b81b71580182651518c0b5420dcc3d197a4384
AlexGralewski <gralewski.aleksander@gmail.com>
2021-02-05 14:56:32 +0100

    Changes made to publish page via gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { HashRouter as Router, Route } from "react-router-dom"
 
 import Menu from "./components/Menu"
 import BetSelection from "./components/BetSelection"
